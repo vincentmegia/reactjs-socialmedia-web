679826381ada4e7de3d6fcf4bef481b1981e4462
vincentmegia <vincent.megia@gmail.com>
2021-04-04 17:06:06 +0800

    refactored menusidebar

--- src/sidebar/MenuSidebar.tsx
+++ src/sidebar/MenuSidebar.tsx
@@ -1,47 +1,52 @@
 import React, { memo, PropsWithChildren } from "react"
-import { Button } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import DebugRender from "../tools/DebugRender"
 import "./MenuSidebar.css"
 import { IMenuSidebarProps } from "./widgetsidebar-types"
+import { v4 as uuidv4 } from 'uuid';
 
 const equals = (prevProps: Readonly<PropsWithChildren<IMenuSidebarProps>>,
 	nextProps: Readonly<PropsWithChildren<IMenuSidebarProps>>) => {
 	return prevProps.controlid === nextProps.controlid
 }
 
+interface IMenuItemProps {
+	className: string
+	menuText?: string
+}
+
+const MenuItem = ({className, menuText}: IMenuItemProps) => {
+	return <li className="sidebarlist-item">
+		<div className="sidebarlist-item-container">
+			<div className="sidebarlist-item-container-cell-icon"><i className={`${className} fa-2x`}/></div>
+			<div>{menuText ?? ""}</div>
+		</div>
+	</li>
+}
+
 const MenuSidebar = ({ controlid, onMenuCallback }: IMenuSidebarProps) => {
 	const onClick = (e: any) => onMenuCallback("sdfsd")
+	const items = [
+		{ className: "fab fa-earlybirds" },
+		{ className: "fas fa-house-user", menuText: "Home" },
+		{ className: "fas fa-hashtag", menuText: "Notifications" },
+		{ className: "fas fa-hashtag", menuText: "Messagers" },
+		{ className: "fas fa-bookmark", menuText: "Bookmarks" },
+		{ className: "fas fa-list", menuText: "List" },
+		{ className: "fas fa-user", menuText: "Profile" },
+		{ className: "fas fa-ellipsis-h", menuText: "More" },
+		{ className: "fas fa-ellipsis-h", children: <Button onClick={onClick}>Tweet</Button> }
+	]
 
 	return (
 		<div id="menu-sidebar" style={{ backgroundColor: "red" }}>
 			<ul className="sidebarlist">
-				<li className="sidebarlistitem">
-					<i className="fab fa-earlybirds fa-2x"></i>
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-house-user fa-2x"></i> Home
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-hashtag fa-2x"></i> Notifications
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-envelope fa-2x"></i> Messagers
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-bookmark fa-2x"></i> Bookmarks
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-list fa-2x"></i> List
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-user fa-2x"></i> Profile
-				</li>
-				<li className="sidebarlistitem">
-					<i className="fas fa-ellipsis-h fa-2x"></i> More
-				</li>
-				<li className="sidebarlistitem">
-					<Button onClick={onClick}>Tweet</Button>
-				</li>
+				{
+					items.map(item => <MenuItem
+						key={uuidv4()}
+						className={item.className} 
+						menuText={item.menuText} />)
+				}
 			</ul>
 			<DebugRender />
 		</div>
